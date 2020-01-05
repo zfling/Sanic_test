eac9475107c710f8c0de23a4be7be7f13f7e0e2f
zfling <18609265862@163.com>
2020-01-05 20:29:18 +0800

    hello

---
+++ demo/hello.py
@@ -0,0 +1,11 @@
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic()
+
+@app.route("/")
+async def test(request):
+    return json({"hello": "world"})
+
+if __name__ == "__main__":
+    app.run(host="192.168.3.10", port=8000)
